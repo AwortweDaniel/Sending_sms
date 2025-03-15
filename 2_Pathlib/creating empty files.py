9165bf1e4324d27a358ccaa55cad69110fbb058b
Awortwe Daniel <123452581+AwortweDaniel@users.noreply.github.com>
2025-03-15 13:59:21 +0000

    Delete 2_Pathlib directory

--- 2_Pathlib/creating empty files.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-root_dir = Path("creating empty files")
-
-for i in range(2, 16):
-    filename = str(i) + ".txt"
-    filepath = root_dir/ Path(filename)
-    filepath.touch()
